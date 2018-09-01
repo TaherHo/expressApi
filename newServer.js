4da2576d3243c821a1482cda0797c3c3cc79df81
taHo <hoseininejad@gmail.com>
2018-09-01 23:52:27 +0200

    🔧edited server configuration for express

--- newServer.js
+++ newServer.js
@@ -2,21 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const port= process.env.PORT || 3030;
+const port= process.env.PORT || 3031;
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json()); 
 
-// mongoose.connect("mongodb://localhost:27017/helloApi");
+mongoose.connect("mongodb://localhost:27017/helloApi", { useNewUrlParser: true });
 
-var router = express.Router();
-app.use('/api', router);
+const welcome = express.Router();
+const documentaion = express.Router();
 
-app.get('/', (req, res) => {
+welcome.get('/', (req, res) => {
     res.send("Hello from my second API");
-    console.log(port);
 });
+app.use('/api', welcome);
+
+documentaion.get('/', (req, res) => {
+    res.json({"dataType" : "Documentaion:" ,
+                "dataObject": "docu"});
+});
+app.use('/doc', documentaion);
+
+
 app.listen(port, ()=> {
     console.log("New Server listen on port: " + port);
 });
